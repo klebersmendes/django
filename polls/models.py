21c0d002b95dd4316719666bae523086111ae41d
kleber.santos <kleber.santos@sanp.com.br>
2021-10-08 11:27:48 -0300

    initial changes

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField('data da publicacao')
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - timedelta(days=1) <= self.pub_date <= now
